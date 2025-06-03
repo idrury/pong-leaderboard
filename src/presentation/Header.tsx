3439c921094849c5bb87573c9b0418b8d62598ee
J-Mann-123 <jmann5326@gmail.com>
2025-06-03 17:05:33 +0930

    Added rickroll link

--- src/presentation/Header.tsx
+++ src/presentation/Header.tsx
@@ -251,7 +251,12 @@ export default function Header ({ }) {
 
       <div className="boxed between w100 pt1 pb1">
         <div></div>
-        <p style={{ fontWeight: 500, fontSize: 'large' }}>Ping-Pong-A-Thon</p>
+        <p
+          onClick={() => window.open('https://www.youtube.com/watch?v=xvFZjo5PgG0', '_blank')}
+          style={{ fontWeight: 500, fontSize: 'large', cursor: 'pointer' }}
+        >
+          Ping-Pong-A-Thon
+        </p>
         <div style={{ display: 'flex', alignItems: 'center', }}>
           <button className="accentButton" onClick={() => setEditActive(true)}>
             + Add rally
