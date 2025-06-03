90e9292939a1b1a88117666a3aacb39fd1b9dc00
J-Mann-123 <jmann5326@gmail.com>
2025-06-03 12:19:09 +0930

    Added Recent scores

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import './App.css'
 import HighscoreCard from './presentation/HighscoreCard';
+import RecentScores from './presentation/RecentScores';
 
 
 function App () {
 
   const record_types = ['rec1', 'rec2', 'rec3'];
+  const records = [
+    {
+      type: 'rec1',
+      number: 100,
+      name: 'Player1'
+    },
+    {
+      type: 'rec2',
+      number: 100,
+      name: 'Player2'
+    },
+    {
+      type: 'rec3',
+      number: 100,
+      name: 'Player3'
+    },
+  ];
 
 
   return (
@@ -12,6 +30,11 @@ function App () {
       <div>
         {record_types.map((recordType, index) => (
           <HighscoreCard key={index} recordType={recordType} />
+        ))}
+      </div>
+      <div>
+        {records.map((record, index) => (
+          <RecentScores key={index} record={record} />
         ))}
       </div>
     </>
